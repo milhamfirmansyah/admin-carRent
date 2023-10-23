5f0a87febf06350d9fb8872d493b394d028b681f
Ilham Firmansyah <milhamfirmansyah5@gmail.com>
2023-10-23 15:21:19 +0700

    create sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { useState } from 'react';
+import { HomeIcon, TruckIcon } from '@heroicons/react/24/outline';
+
+function Sidebar() {
+  return (
+    <div className="w-[70px] h-[902px] bg-[#0D28A6] flex flex-col items-center fixed z-10">
+      <div className="w-[34px] h-[34px] bg-[#CFD4ED] my-[18px]"></div>
+      <div className="flex flex-col items-center py-[20px] cursor-pointer hover:bg-[#FFF] hover:bg-opacity-30 w-full">
+        <HomeIcon className="h-[24px] w-[24px] text-[#FFFFFF]" />
+        <p className="text-[#FFF] text-[12px] font-sans font-bold leading-[18px]">Dashboard</p>
+      </div>
+      <div className="flex flex-col items-center py-[20px] cursor-pointer hover:bg-[#FFF] hover:bg-opacity-30 w-full">
+        <TruckIcon className="h-[24px] w-[24px] text-[#FFFFFF]" />
+        <p className="text-[#FFF] text-[12px] font-sans font-bold leading-[18px]">Cars</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Sidebar from './components/Sidebar'
 
 export default function Home() {
   return (
     <>
-      <div>Test</div>
+      <div>
+        <Sidebar />
+      </div>
     </>
   )
 }
