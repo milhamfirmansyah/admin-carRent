201925555e29aab9451bc8c10d2dfed0f1df7e39
Ilham Firmansyah <milhamfirmansyah5@gmail.com>
2023-11-13 08:58:45 +0700

    remove role

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,15 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/react/24/solid';
 
 function Navbar() {
-  const [role, setRole] = useState('');
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      const roleAdmin: any = localStorage.getItem('roleAdmin');
-      setRole(roleAdmin);
-    }
-  }, []);
-
+ 
   return (
     <div className="pl-[94px] pr-6 py-[18px] flex justify-between items-center bg-[#fff] shadow-sm fixed top-0 right-0 left-0 z-10">
       <div className="w-[100px] h-[34px] bg-[#CFD4ED]"></div>
@@ -24,8 +16,8 @@ function Navbar() {
           <button className="border w-[61px] h-[36px] text-sm font-bold text-[#0D28A6] rounded-sm border border-[#0D28A6]">Search</button>
         </div>
         <div className="flex gap-2 items-center">
-          <div className="w-[38px] h-[38px] rounded-full bg-[#CFD4ED] flex justify-center items-center text-[#151515] text-base font-bold">{role.charAt(0)}</div>
-          <p className="text-[#151515] text-sm">{role}</p>
+          <div className="w-[38px] h-[38px] rounded-full bg-[#CFD4ED] flex justify-center items-center text-[#151515] text-base font-bold">A</div>
+          <p className="text-[#151515] text-sm">Admin</p>
           <ChevronDownIcon className="w-5 h-5 text-[#151515]" />
         </div>
       </div>
